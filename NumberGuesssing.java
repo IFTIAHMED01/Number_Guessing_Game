084859adb2d913486cfc92f28fb094aa6249cf6d
IFTI AHMED <97821295+IFTIAHMED01@users.noreply.github.com>
2026-06-03 03:29:31 +0600

    Update NumberGuesssing.java

--- NumberGuesssing.java
+++ NumberGuesssing.java
@@ -5,22 +5,30 @@ public class NumberGuesssing {
     public static void main(String[] args) {
      Scanner eri = new Scanner(System.in);
 
-     System.out.print("Please guess the Number: ");
+     int i=0;
+     do{
+      System.out.print("Please guess the Number: ");
 
-     int num = eri.nextInt();
+      int num = eri.nextInt();
 
-     Random rd = new Random();
-     int rand = rd.nextInt();
-     System.out.println("Random Number: "+ rand);
+      Random rd = new Random();
+      int rand = rd.nextInt();
+      System.out.println("Random Number: "+ rand);
      
-     if (num==rand){
-        System.out.println("Congratulations, you have guessed the correct number");
-     }
-     else if(num > rand){
-        System.out.println("Too High");
-     }
-     else{
-        System.out.println("Too Low");
-     }
-  }
+      if (num==rand){
+         System.out.println("Congratulations, you have guessed the correct number");
+      }
+      else if(num > rand){
+         System.out.println("Too High");
+         System.out.println("Please Try Again!");
+      }
+      else{
+         System.out.println("Too Low");
+         System.out.println("Please Try Again!");
+      }
+      i++;
+     } while(i<3);
+
+     System.out.println("Thank You for playing the Game!");
+   }
 }
